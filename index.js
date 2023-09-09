5ae15300cdd18204347a415edf284c5401f0dffb
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-09-09 17:53:27 +0100

    fix another bug

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const currentDate = new Date();
-
-        const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "WednesDay", "Thursday", "Friday"];
-
-        const currentDay = dayOfTheWeek[currentDate.getUTCDay()];
-        const utcTime = currentDate.toUTCString().substring(16, 22);
-
-        document.getElementById("currentDay").textContent = currentDay
-        document.getElementById("utcTime").textContent = utcTime
+function getCurrentDay() {
+        let currentDate = new Date();
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let currentDayIndex = currentDate.getDay();
+        let currentDayName = days[currentDayIndex];
+    
+        let utcHours = currentDate.getUTCHours();
+        let utcMinutes = currentDate.getUTCMinutes();
+        let utcSeconds = currentDate.getUTCSeconds();
+    
+        // Ensure single-digit minutes and seconds are displayed with leading zeros
+        utcMinutes = utcMinutes < 10 ? "0" + utcMinutes : utcMinutes;
+        utcSeconds = utcSeconds < 10 ? "0" + utcSeconds : utcSeconds;
+    
+        let utcTime = utcHours + ":" + utcMinutes + ":" + utcSeconds;
+    
+        document.getElementById("currentDay").innerHTML = currentDayName;
+        document.getElementById("utcTime").innerHTML = utcTime;
+    }
+    
+    window.onload = getCurrentDay;
